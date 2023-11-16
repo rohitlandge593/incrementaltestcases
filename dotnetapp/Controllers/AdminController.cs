06ded2387901c8e8da7a6aa5740c87972b2569f3
Iamneo <techadmin@iamneo.ai>
2023-11-16 18:16:27 +0000

    Backup Commit

--- dotnetapp/Controllers/AdminController.cs
+++ dotnetapp/Controllers/AdminController.cs
@@ -5,12 +5,14 @@ using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using dotnetapp.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace dotnetapp.Controllers
 {
 
     
-
+    [ApiController]
+    [Route("api/[controller]")]
     public class AdminController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
@@ -18,6 +20,29 @@ namespace dotnetapp.Controllers
         public AdminController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        [Route("GetPlayer")]
+        public IActionResult Get()
+        {
+            var playerList = _context.Players.ToList();
+            return Ok(playerList);
+        }
+        [HttpPost]
+        [Route("Post")]
+        public IActionResult Post(Player player)
+        {
+            _context.Players.Add(player);
+            _context.SaveChanges();
+            return CreatedAtAction("Get",new {id=player.Id},player);
+        } 
+        [HttpGet]
+        [Route("GetTeams")]
+        public IActionResult GetTeams()
+        {
+            var teamList= _context.Teams.ToList();
+            return Ok(teamList);
         }
 
         
--- dotnetapp/Controllers/TeamController.cs
+++ dotnetapp/Controllers/TeamController.cs
@@ -2,21 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using dotnetapp.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnetapp.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class PlayerController : ControllerBase
+    public class TeamController : ControllerBase
     {
-        ApplicationDbContext db;
-        public PlayerController(ApplicationDbContext context)
-        {
-            this.db=context;
-        }
-
         
     }
 }
