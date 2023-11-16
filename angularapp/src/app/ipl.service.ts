f50cf8debbb021fbbac80ab105be044a08f50229
Iamneo <techadmin@iamneo.ai>
2023-11-16 20:02:15 +0000

    Backup Commit

--- angularapp/src/app/ipl.service.ts
+++ angularapp/src/app/ipl.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
+import{ HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IplService {
-  private url=''
+  private url='https://8081-afdcbdaefbcfacbbecabcdadeafbbdcaeafe.premiumproject.examly.io/api/Admin/GetPlayer';
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  getAllPlayers():Observable<any[]>
+  {
+    return this.http.get<any[]>(this.url+'/GetPlayer')
+  }
+  
 }
