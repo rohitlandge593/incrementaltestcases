0bb1e81995b277722a681b9a069cd4378768aad1
Iamneo <techadmin@iamneo.ai>
2023-11-16 20:16:46 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetPlayerComponent } from './get-player/get-player.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetPlayerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angularapp/src/app/get-player/get-player.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { IplService } from '../ipl.service';
+
+@Component({
+  selector: 'app-get-player',
+  templateUrl: './get-player.component.html',
+  styleUrls: ['./get-player.component.css']
+})
+export class GetPlayerComponent implements OnInit {
+  playerdata:any[]=[]
+  constructor(private service:IplService) {
+    this.service.getAllPlayers().subscribe(data=>{this.})
+   }
+
+  ngOnInit(): void {
+  }
+
+}
