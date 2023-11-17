7aad865bceb8ac994ddae33377a89ac1bd357e70
Iamneo <techadmin@iamneo.ai>
2023-11-17 08:34:05 +0000

    Backup Commit

--- angularapp/src/app/addplayer/addplayer.component.ts
+++ angularapp/src/app/addplayer/addplayer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IplService } from '../ipl.service';
 
 @Component({
   selector: 'app-addplayer',
@@ -6,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addplayer.component.css']
 })
 export class AddplayerComponent implements OnInit {
-  
+  age:number
+  name:string
+  teamId:number
+  category:string
+  biddingPrice:number
 
-  constructor() { }
+  constructor(private service:IplService) { }
+  handleSave()
+    {
+        let data: object={age:this.age, name:this.name, departmentId:this.departmentId, designationId: this.designationId}
+        console.log(this.service.addPlayers(data));
+    }
 
   ngOnInit(): void {
   }
