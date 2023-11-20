d4e08eef8e91da914fbc2aa9b53a170d5c2b6636
Iamneo <techadmin@iamneo.ai>
2023-11-20 13:56:48 +0000

    Backup Commit

--- angularapp/src/app/getplayerbyid/getplayerbyid.component.ts
+++ angularapp/src/app/getplayerbyid/getplayerbyid.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IplService } from '../ipl.service';
 import { IPlayer } from '../Model/iplayer';
 import { ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-getplayerbyid',
@@ -10,13 +11,21 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class GetplayerbyidComponent implements OnInit {
 
-  playerdata:IPlayer
-  id:number
+  playerdata:object ={};
+  
+  id:any
+  isReady = false;
+ 
   constructor(private service:IplService, private ar:ActivatedRoute) { }
-  getPlayerDetails():void{
+  getPlayerDetails(playerform:NgForm):void{
     // const pid=this.ar.snapshot.paramMap.get('id')
     // this.id=Number(pid)
-    this.service.getPlayerById(this.id).subscribe(data=>{this.playerdata=data});
+    this.id=playerform.controls['id'].value;
+    this.service.getPlayerById(this.id).subscribe(data=>{
+      this.playerdata=data; 
+      this.isReady= true;
+      console.log(this.playerdata);
+    })
   }
   
 
