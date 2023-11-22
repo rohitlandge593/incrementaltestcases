14fdd5e3227e348cf52fcceb9ca1fee4f605ce26
Iamneo <techadmin@iamneo.ai>
2023-11-22 09:28:59 +0000

    Backup Commit

--- angularapp/src/app/deleteteams/deleteteams.component.ts
+++ angularapp/src/app/deleteteams/deleteteams.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ITeam } from '../Model/iteam';
 import { IplService } from '../ipl.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-deleteteams',
@@ -12,10 +13,14 @@ export class DeleteteamsComponent implements OnInit {
 
   teamdata:ITeam={TeamId:0,Name:''}
 
-  constructor(private service:IplService,private route:Router,private ar:ActivatedRoute) {
+  constructor(private fb:FormBuilder, private service:IplService,private route:Router,private ar:ActivatedRoute) {
     const no=this.ar.snapshot.paramMap.get('id')
     this.teamdata.TeamId=Number(no)
    }
+   deleteTeamForm=this.fb.group({
+    TeamId:[''],
+    Name:['']
+   })
 
   deleteData(team:ITeam)
   {
